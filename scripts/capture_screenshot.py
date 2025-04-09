12a560d7d878c5373074f4508a80f3f0c40b6802
ezdakit <ezdakit@gmail.com>
2025-04-09 13:07:13 +0200

    Update capture_screenshot.py

--- scripts/capture_screenshot.py
+++ scripts/capture_screenshot.py
@@ -4,7 +4,7 @@ import subprocess
 
 def capture_screenshot(acestream_url, output_folder):
     # Inicia el stream de acestream
-    stream_command = f"acestreamengine --client-console {acestream_url}"
+    stream_command = f"./AceStream-3.1.49-v2.2.AppImage --client-console {acestream_url}"
     process = subprocess.Popen(stream_command, shell=True)
     
     # Espera 10 segundos
@@ -18,7 +18,7 @@ def capture_screenshot(acestream_url, output_folder):
     process.terminate()
 
 if __name__ == "__main__":
-    acestream_url = "acestream://b5842718859345a596107ab8e6b24d7bfa2d617e"
+    acestream_url = "acestream://b5842718859345a596107ab8e6b24d7bfa2d617e"  # Reemplaza con tu URL de acestream
     output_folder = "ace_kanalak"
     os.makedirs(output_folder, exist_ok=True)
     capture_screenshot(acestream_url, output_folder)
