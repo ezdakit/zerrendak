f00e24ca346e6b0d923a3c659715d973db9a598a
ezdakit <ezdakit@gmail.com>
2025-04-09 13:46:48 +0200

    Update capture_screenshot.py

--- scripts/capture_screenshot.py
+++ scripts/capture_screenshot.py
@@ -14,7 +14,14 @@ def capture_screenshot(acestream_id, output_folder):
     os.system(screenshot_command)
 
 if __name__ == "__main__":
-    acestream_id = os.getenv('ACESTREAM_ID')  # Obtiene la ID de Ace Stream de las variables de entorno
+    # Obtiene la ID de Ace Stream de las variables de entorno
+    acestream_id = os.getenv('ACESTREAM_ID')
+    
+    # Define la carpeta de salida
     output_folder = "ace_kanalak"
+    
+    # Crea la carpeta de salida si no existe
     os.makedirs(output_folder, exist_ok=True)
+    
+    # Llama a la función para capturar la captura de pantalla
     capture_screenshot(acestream_id, output_folder)
