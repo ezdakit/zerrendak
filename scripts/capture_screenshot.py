d0db7214d79cac0a4def62f502c4bf8cb1585000
ezdakit <ezdakit@gmail.com>
2025-04-09 13:35:22 +0200

    Update capture_screenshot.py

--- scripts/capture_screenshot.py
+++ scripts/capture_screenshot.py
@@ -6,19 +6,12 @@ def capture_screenshot(acestream_id, output_folder):
     # Construye la URL de Ace Stream
     acestream_url = f"acestream://{acestream_id}"
     
-    # Inicia el stream de Ace Stream
-    stream_command = f"/usr/local/bin/acestream --client-console {acestream_url}"
-    process = subprocess.Popen(stream_command, shell=True)
-    
     # Espera 10 segundos
     time.sleep(10)
     
     # Toma una captura de pantalla usando ffmpeg
     screenshot_command = f"ffmpeg -i http://127.0.0.1:6878/ace/getstream -vframes 1 {output_folder}/screenshot.png"
     os.system(screenshot_command)
-    
-    # Termina el proceso de Ace Stream
-    process.terminate()
 
 if __name__ == "__main__":
     acestream_id = os.getenv('ACESTREAM_ID')  # Obtiene la ID de Ace Stream de las variables de entorno
